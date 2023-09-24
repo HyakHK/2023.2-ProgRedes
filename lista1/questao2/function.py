29ce7328c4c98f665829de120a5be4005b60f7f1
Hyak <teuso784@gmail.com>
2023-09-24 16:16:47 -0300

    questao 2, eletric boogaloo

---
+++ lista1/questao2/function.py
@@ -0,0 +1,102 @@
+
+
+
+import array
+
+
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r') as arquivo:
+            content = arquivo.read()
+
+
+        lido = True
+    except:
+        lido = False
+        content = None
+    
+    return lido and content
+
+def ordena_lista(lido):
+    try:
+
+        match str(input('Tipo de ordenação(todas maiusculas)')):
+            case 'BUBBLE':
+                lista = ordena_bubble(lido)
+            case 'INSERTION':
+                lista = ordena_insertion(lido)
+            case 'SELECTION':
+                lista = ordena_selection(lido)
+            case 'QUICK':
+                lista = ordena_quick(lido)
+        ordenada = True
+    except:
+        ordenada = False
+        lista = None
+    return ordenada and lista
+
+
+def ordena_bubble(lido):
+    n = len(lido)
+    if n <= 1:
+        swapped = False
+        for i in range(n-1):
+            for j in range(0, n-i-1):
+                if lido[j] > lido[j + 1]:
+                    swapped = True
+                    lido[j], lido[j + 1] = lido[j + 1], lido[j]
+            if not swapped:
+                return
+
+def ordena_insertion(lido):
+    n = len(lido)
+    #Retornar se possuir 1 elemento ou menor
+    if n <= 1:
+        return
+    for i in range(1, n):
+        key = lido[i]
+        j = i-1
+        while j >= 0 and key < lido[j]: #movimentar maior para o lado
+            lido[j + 1] = lido[j] #mevo para dito lado(é a direita so para saber)
+            j =- 1
+        lido[j + 1] = key
+
+def ordena_selection(lido):
+    size = len(lido)
+
+    for ind in range(size):
+        min_index = ind
+        for j in range(ind + 1, size): #Selecionar menor
+            if array[j] < array[min_index]:
+                min_index = j
+            #posicionamento
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+
+def partition(array,low,high):
+    pivot = array[high]
+    i = low - 1
+    #comparar com pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+            #se maior que pivot trocar por elemento apontado
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    #trocar elementon pivot por maior
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    #Resumo conta no i e usa numero i como comparação para o maior
+    return i + 1
+
+
+
+def ordena_quick(lido):
+    high = len(lido) - 1
+    low = 0
+
+    if low < high :
+        pi = partition(lido, low, high)
+        #chamada esquerda
+        ordena_quick(lido, low, pi - 1)
+        #chamada direta
+        ordena_quick(lido, pi - 1, high)
+
+
--- lista1/questao2/l1-q2.py
+++ lista1/questao2/l1-q2.py
@@ -1,9 +1,25 @@
 import os,sys
-
+from function import *
 try:
     hereAnt = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    q1A = str(input('Digite o nome do arquivo localizado'))
-    q1L = str(input('Digite o nome da lista'))
-    print(os.path.join(hereAnt,q1A,q1L))
+    q1P = str(input('Digite o nome da pasta: '))
+    q1L = str(input('Digite o nome da lista: '))
+    if q1L.endswith('.txt'):
+        ...
+    else:
+        q1L = q1L + '.txt'
+    nome_arquivo = os.path.join(hereAnt,'questao1',q1P,q1L)
+    try:
+        f = open(nome_arquivo)
+        f.close
+    except:
+        print('Arquivo não existente')
+        exit()
+        
+    
+    lido = ler_arquivo(nome_arquivo)
+
+    lista = ordena_lista(lido)
+    print(lista)
 except:
     print('Erro:' ,sys.exc_info()[0] + "Tente novamente")
