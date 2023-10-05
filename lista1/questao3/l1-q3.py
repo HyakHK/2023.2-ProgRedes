4dea6917f1e1f90e57f16fac600dfba471494f70
Hyak <teuso784@gmail.com>
2023-10-05 11:23:43 -0300

    o inicio de algo
    n foi pouran

--- lista1/questao3/l1-q3.py
+++ lista1/questao3/l1-q3.py
@@ -5,5 +5,7 @@ here = os.path.dirname(os.path.abspath(__file__))
 dump_locate = os.path.join(here,'tcp_dump','cap1.dump')
 
 with open (dump_locate,'rb') as yesn:
-    print(yesn.read())
-
+    data = yesn.read(21)
+    data.split()
+    for l in range(len(data)):
+        print(data[l])
