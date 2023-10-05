17b7d9f4a90a1ece78c41597d4d86d4cdde720c9
Hyak <teuso784@gmail.com>
2023-10-05 10:31:20 -0300

    up

--- lista1/questao3/l1-q3.py
+++ lista1/questao3/l1-q3.py
@@ -2,4 +2,8 @@ import os
 from funcao import *
 
 here = os.path.dirname(os.path.abspath(__file__))
-dump_locate = os.path.join(here,'tcp_dump')
+dump_locate = os.path.join(here,'tcp_dump','cap1.dump')
+
+with open (dump_locate,'rb') as yesn:
+    print(yesn.read())
+
