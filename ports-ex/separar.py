2a864dfea3fd05ecf5ac2d511bdebeb31d964ac3
Hyak <teuso784@gmail.com>
2023-11-09 10:02:38 -0300

    Update separar.py

--- ports-ex/separar.py
+++ ports-ex/separar.py
@@ -1,11 +1,14 @@
-import sys
+import sys, os
 def criarList():
     try:
-        file = open("C:/Users/HOME/Downloads/ports-ex/tabela.csv", "r", encoding='utf-8')
+        here = os.path.dirname(os.path.abspath(__file__))
+        file = open(here + "/tabela.csv", "r", encoding='utf-8')
         file = file.read()
         list = file.split('\n')
         list.remove('')
     except:
         print(f"Erro: {sys.exc_info()[0]}")
     else:
+        
         return list
+criarList()
