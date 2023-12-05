df64df5ee58f1509ee7e4482eaf9c0863e297a25
Hyak <teuso784@gmail.com>
2023-12-05 10:09:28 -0300

    Update socket_constants.py

--- projeto_disciplina/socket_constants.py
+++ projeto_disciplina/socket_constants.py
@@ -1,6 +1,6 @@
 HOST_SERVER = 'localhost'   # Definindo o IP do servidor
 SOCKET_PORT = 50000         # Definindo a porta
-API_KEY = '6684795612:AAH0dfm-83OAZSBV11CZleKx0RNHjQC0cR4' # Token API bot telegram
+API_KEY = 'aa' # Token API bot telegram
 
 BUFFER_SIZE = 512           # Definindo o tamanho do buffer
 CODE_PAGE   = '*UTF-8'       # Definindo a página de códigos de caracteres-
