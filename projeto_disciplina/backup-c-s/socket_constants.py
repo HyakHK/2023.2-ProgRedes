3aeaf29eded64f2f56ec2f63c0b1ef31049c7221
Hyak <teuso784@gmail.com>
2023-12-27 09:19:23 -0300

    burrice part 2

--- projeto_disciplina/backup-c-s/socket_constants.py
+++ projeto_disciplina/backup-c-s/socket_constants.py
@@ -1,6 +1,6 @@
 HOST_SERVER = 'localhost'   # Definindo o IP do servidor
 SOCKET_PORT = 50000         # Definindo a porta
-API_KEY = '6865045299:AAGK-H_TOXknuDiC0qI4IoWNQ_jd3o3zxhE' # Token API bot telegram
+API_KEY = 'key' # Token API bot telegram
 
 BUFFER_SIZE = 512           # Definindo o tamanho do buffer
 CODE_PAGE   = '*UTF-8'       # Definindo a página de códigos de caracteres-
